2a8e315f6841ea68f3896e84dd305309ef11a28d
Chase Opsahl <cjopsahl99@gmail.com>
2017-12-11 12:54:49 -0600

    Add files via upload

---
+++ bomb_runner/bomb_modules.py
@@ -0,0 +1,193 @@
+import time, os
+
+def module_one():
+	'''Wires'''
+	os.system('cls')
+	wires_cut = 0
+	wires = [
+		['|','|','|','|','|'],
+		['|','|','|','|','|'],
+		['|','|','|','|','|'],
+		['|','|','|','|','|'],
+		['1','2','3','4','5']
+	]
+	def print_wires(array):
+		for y in range(0,5):
+			for x in range(0,5):
+				print(array[y][x] + ' ', end = '')
+			print()
+	print_wires(wires)
+	print("You must cut three wires in the correct order to pass!")
+	print("HINT: ADD these three numbers from smallest to largest,")
+	print("and they equal 8")
+	while wires_cut < 3:
+		if wires_cut == 0:
+			wire = input("Cut wire: ")
+			if wire == '1':
+				print("correct")
+				time.sleep(1)
+				wires[2][0] = '*'
+				wires_cut += 1
+				os.system('cls')
+				print_wires(wires)
+			else:
+				time.sleep(2)
+				print("BOOM!")
+				return False
+		if wires_cut == 1:
+			wire = input("Cut wire: ")
+			if wire == '2':
+				print("correct")
+				time.sleep(1)
+				wires[2][1] = '*'
+				wires_cut += 1
+				os.system('cls')
+				print_wires(wires)
+			else:
+				time.sleep(1)
+				print("BOOM!")
+				return False
+		if wires_cut == 2:
+			wire = input("Cut wire: ")
+			if wire == '5':
+				time.sleep(2)
+				print("correct")
+				time.sleep(1)
+				wires[2][4] = '*'
+				wires_cut += 1
+				os.system('cls')
+				print_wires(wires)
+			else:
+				time.sleep(2)
+				print("BOOM!")
+				return False
+		if wires_cut == 3:
+			print("BOMB DEFUSED")
+			time.sleep(2)
+			return True
+
+def module_two():
+	'''NMAP MODULE'''
+	os.system('cls')
+	attempts = 1
+	print("what port is closed?")
+	time.sleep(3)
+	os.system("cls")
+	while attempts <= 3:
+		nmap_cmd = input("root@kali:~# ")
+		if nmap_cmd == 'nmap localhost' or nmap_cmd == 'nmap 127.0.0.1':
+			print("PORT      STATE:  SERVICE")
+			print("21/tcp    OPEN    ftp")
+			print("23/tcp    OPEN    telnet")
+			print("80/tcp    OPEN    http")
+			print("420/tcp   CLOSED  abyss")
+			print("1337/tcp  OPEN    hax.exe")
+			print("3306/tcp  OPEN    mysql")
+			print("\nType the port number then hit enter")
+			while attempts <=3:
+				kill_cmd = input("root@kali:~# ")
+				if kill_cmd == '420':
+					print("CORRECT")
+					time.sleep(1)
+					print("BOMB DEFUSED")
+					time.sleep(1)
+					return True
+				else:
+					print("Invalid input")
+					time.sleep(1)
+					attempts += 1
+		else:
+			print("Wrong Input!")
+			time.sleep(1)
+			attempts +=1
+	print("BOOM")
+	time.sleep(1)
+	return False
+
+def module_three():
+	"""ALGEBRA PROBLEM"""
+	print("Solve this quadratic using any method,")
+	print("x^2 - 9x + 18 = 0")
+	x1 = input("x = ")
+	x2 = input("x = ")
+	if x1 == '-6':
+		if x2 == '-3':
+			print("CORRECT")
+			print("BOMB DEFUSED")
+			time.sleep(1)
+			return True
+		else:
+			return False
+			
+	if x1 == '-3':
+		if x2 == '-6':
+			print("CORRECT")
+			print("BOMB DEFUSED")
+			time.sleep(1)
+			return True
+		else:
+			print("BOOM")
+			time.sleep(1)
+			return False
+	
+	if x1 != '-6' and x1 != '-3':
+		print("BOOM")
+		time.sleep(1)
+		return False
+		
+
+def module_four():
+	'''Song Lyric Guess'''
+	# try to shorten with for loop
+	os.system("cls")
+	print("Complete the song by filling in the blanks:\n")
+	print("Somebody once told me the _____ is gonna roll me,")
+	print("I ain't the ________ tool in the shed.")
+	print("She was looking kinda dumb with her _______ and her thumb")
+	print("in the _____ of an L on her _________\n")
+	w1 = input("Enter the word that goes in the 1st blank: ").lower()
+	w2 = input("Enter the word that goes in the 2nd blank: ").lower()
+	w3 = input("Enter the word that goes in the 3rd blank: ").lower()
+	w4 = input("Enter the word that goes in the 4th blank: ").lower()
+	w5 = input("Enter the word that goes in the 5th blank: ").lower()
+	
+	if w1 == 'world' and w2 == 'sharpest' and w3 == 'finger' and w4 == 'shape' and w5 == 'forehead':
+		os.system("cls")
+		print("YOU ARE CORRECT\nAll Star By Smashmouth\n")
+		print("Somebody once told me the '{}' is gonna roll me,".format(w1))
+		print("I ain't the '{}' tool in the shed.".format(w2))
+		print("She was looking kinda dumb with her '{}' and her thumb".format(w3))
+		print("in the '{}' of an L on her '{}'".format(w4,w5))
+		time.sleep(2)
+		print("BOMB DEFUSED")
+		return True
+	else:
+		print("Incorrct")
+		time.sleep(1)
+		print("BOOM")
+		time.sleep(1)
+		return False
+	
+	
+def module_five():
+	'''What are the missing numbers in the fibonacci sequence'''
+	os.system('cls')
+	print("What are the missing numbers? ")
+	print("0, 1, 1, 2, 3, 5, 8, __, 21, __, 55, __, 144,... ")
+	
+	num1 = input("Enter 1st num: ")
+	num2 = input("Enter 2nd num: ")
+	num3 = input("Enter 3rd num: ")
+	
+	if num1 == '13' and num2 == '34' and num3 == '89':
+		print("Correct")
+		print("BOMB DEFUSED")
+		time.sleep(1)
+		return True
+	else:
+		print("Incorrct")
+		time.sleep(1)
+		print("BOOM")
+		time.sleep(1)
+		return False
+	
---
+++ bomb_runner/bomb_runner.py
@@ -0,0 +1,187 @@
+import os
+import random
+import time
+import bomb_modules as bm
+import msvcrt
+
+gameBoard = [
+	['+','+','+','+','+','+','+','+','+','+','+','+','+'], # x 1-11
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'], # y 1-3
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+','+','+','+','+','+','+','+','+','+','+','+','+'], # 1-11
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'], # y 6 or 7
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+','+','+','+','+','+','+','+','+','+','+','+','+'], # x 1-11
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'], # y 10 or 11
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','+'],
+	['+','+','+','+','+','+','+','+','+','+','+','+','+']
+]
+
+'''Global Variables'''
+modules = [1,2,3,4,5]
+plyr_x = 6
+plyr_y = 12
+bombs_defused = 0
+lives = 3
+flag = 1
+
+bomb1x = random.randrange(1,12)
+bomb1y = random.randrange(10,12)	
+bomb2x = random.randrange(1,12)
+bomb2y = random.randrange(6,8)	
+bomb3x = random.randrange(1,12)
+bomb3y = random.randrange(1,4)	
+
+
+def intro():
+	os.system('cls')
+	print("__________              ___.     __________                                  ")
+	print("\______   \ ____   _____\_ |__   \______   \__ __  ____   ____   ___________ ")
+	print(" |    |  _//  _ \ /     \| __ \   |       _/  |  \/    \ /    \_/ __ \_  __ \\")
+	print(" |    |   (  <_> )  Y Y  \ \_\ \  |    |   \  |  /   |  \   |  \  ___/|  | \/")
+	print(" |______  /\____/|__|_|  /___  /  |____|_  /____/|___|  /___|  /\___  >__|   ")
+	print("        \/             \/    \/          \/           \/     \/     \/       \n\n")
+	print("    ____           ________                       ____                   __    __")
+	print("   / __ )__  __   / ____/ /_  ____ _________     / __ \____  _________ _/ /_  / /")
+	print("  / __  / / / /  / /   / __ \/ __ `/ ___/ _ \   / / / / __ \/ ___/ __ `/ __ \/ / ")
+	print(" / /_/ / /_/ /  / /___/ / / / /_/ (__  )  __/  / /_/ / /_/ (__  ) /_/ / / / / /  ")
+	print("/_____/\__, /   \____/_/ /_/\__,_/____/\___/   \____/ .___/____/\__,_/_/ /_/_/   ")
+	print("      /____/                                       /_/                           \n\n")
+	input("Press any key to start!")
+
+
+def bomb_init():
+		global bombs_defused, flag
+		global bomb1x, bomb1y, bomb2x, bomb2y, bomb3x, bomb3y
+		gameBoard[bomb1y][bomb1x] = '#'		
+		gameBoard[bomb2y][bomb2x] = '#'
+		gameBoard[bomb3y][bomb3x] = '#'
+
+def bomb_change_sprite():
+	global bomb1x, bomb1y
+	global bombs_defused
+	
+	if bombs_defused == 1:
+		gameBoard[bomb1y][bomb1x] = '*'
+	if bombs_defused == 2:
+		gameBoard[bomb2y][bomb2x] = '*'	
+	if bombs_defused == 3:
+		gameBoard[bomb3y][bomb3x] = '*'
+	
+def print_gameboard():
+	print("W = up\nS = down\nA = left\nD = right")
+	print("Lives: " + str(lives) + "      Bombs Defused: " + str(bombs_defused))
+	for y in range(0 ,14):
+		for x in range(0,13):
+			if y == plyr_y and x == plyr_x:
+				print("P ", end = '')
+			else:
+				print(gameBoard[y][x] + " ", end = "")
+		
+		print()
+
+def move_player():
+	global plyr_x, plyr_y
+	direction = msvcrt.getwch()
+	if direction == 'W' or direction == 'w':
+		if gameBoard[plyr_y - 1][plyr_x] != '+':
+			plyr_y -= 1
+	elif direction == 'S' or direction == 's':
+		if gameBoard[plyr_y + 1][plyr_x] != '+':
+			plyr_y += 1
+	elif direction == 'A' or direction == 'a':
+		if gameBoard[plyr_y][plyr_x - 1] != '+':
+			plyr_x -= 1
+	elif direction == 'D' or direction == 'd':
+		if gameBoard[plyr_y][plyr_x + 1] != '+':
+			plyr_x += 1
+
+def check_for_bombs():
+	global plyr_x, plyr_y
+	os.system("cls")
+	print_gameboard()
+	if gameBoard[plyr_y - 1][plyr_x]=='#' or gameBoard[plyr_y + 1][plyr_x]=='#' or gameBoard[plyr_y][plyr_x - 1]=='#' or gameBoard[plyr_y][plyr_x + 1]=='#':
+		defuse = input("There's a bomb! Type 'DEFUSE': ").upper()
+		if defuse == "DEFUSE":
+			return True
+		elif defuse != "DEFUSE":
+			return False
+
+def defuse_bomb():
+	global bombs_defused, lives, modules
+	select_bomb = random.choice(modules)
+	for n in modules:
+		if select_bomb == n:
+			select_bomb = random.choice(modules)
+	
+	if check_for_bombs() == True:
+		if select_bomb == 1:
+			if bm.module_one() == True:
+				bombs_defused += 1
+				modules.remove(select_bomb)
+			else:
+				lives -= 1
+		elif select_bomb == 2:
+			if bm.module_two() == True:
+				bombs_defused += 1
+				modules.remove(select_bomb)
+			else:
+				lives -= 1
+		elif select_bomb == 3:
+			if bm.module_three() == True:
+				bombs_defused += 1
+				modules.remove(select_bomb)
+			else:
+				lives -= 1
+		elif select_bomb == 4:
+			if bm.module_four() == True:
+				bombs_defused += 1
+				modules.remove(select_bomb)
+			else:
+				lives -= 1
+		elif select_bomb == 5:
+			if bm.module_five() == True:
+				bombs_defused += 1
+				modules.remove(select_bomb)
+			else:
+				lives -= 1
+				
+	if bombs_defused == 1:
+		gameBoard[9][6] = " "
+	elif bombs_defused == 2:
+		gameBoard[5][6] = " "
+								
+def check_if_won():
+	global bombs_defused
+	if bombs_defused == 3:
+		os.system("cls")
+		print_gameboard()
+		print("Congrats! You saved the world!")
+		time.sleep(2)
+		return True
+	else:
+		return False
+
+def main():
+	global lives
+	flag = 0
+	bomb_init()
+	intro()
+	while lives > 0:
+		os.system("cls")
+		print_gameboard()
+		move_player()
+		defuse_bomb()
+		bomb_change_sprite()
+		if check_if_won() == True:
+			return True
+	
+	os.system('cls')
+	print("You died!")
+	print("Lives: " + str(lives) + "      Bombs Defused: " + str(bombs_defused))
+	time.sleep(1)	
+main()
